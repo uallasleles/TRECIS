5ca6a266e982e193b0114eb0b91aa36b1eaad49a
Uallas Leles <uallasleles@hotmail.com>
2022-01-08 05:40:02 -0300

    modularizando método de conexão a database

--- app/app.py
+++ app/app.py
@@ -2,14 +2,11 @@
 import os
 from datetime import datetime
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+from db import get_db
 
 application = Flask(__name__)
 
-application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
-
-mongo = PyMongo(application)
-db = mongo.db
+db = get_db()
 
 @application.route('/')
 def index():
