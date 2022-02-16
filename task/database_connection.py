aed4b4af05854d251b34e98530acb9795b9da594
Uallas Leles <uallasleles@hotmail.com>
2022-02-16 09:30:10 -0300

    Obsoletos ou reestruturados

--- task/database_connection.py
+++
@@ -1,9 +0,0 @@
-import os
-from pymongo import MongoClient
-
-
-def get_db():
-    client = MongoClient()
-    URI = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
-    client = MongoClient(URI)
-    return client.carsdb
